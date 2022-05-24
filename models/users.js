372c1adb47a14362c19dc98f04daff3dd1fe4861
Adam Hsu <projectnba77@gmail.com>
2022-05-24 10:24:16 +0800

    fix: user 1. userModel Schema調整 2. routes 調整 3. 新增用戶註冊API

--- models/users.js
+++ models/users.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, '請輸入您的名字']
-    },
     email: {
       type: String,
-      required: [true, '請輸入您的 Email'],
-      unique: true,
+      trim: true,
       lowercase: true,
+      unique: true,
+      required: [true, 'Email 未填寫'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, '請填寫正確的 Email 信箱'],
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: [true, '暱稱 未填寫'],
+    },
+    photo: {
+      type: String,
+      default: '',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
       select: false
     },
-    photo: String,
+  },
+  {
+    versionKey: false
   }
 );
 
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var router = express.Router()
 
+const handleErrorAsync = require("../service/handleErrorAsync");
 const UserController = require('../controllers/users')
 
 // 取得所有用戶
-router.get('/', UserController.getUser)
+router.get('/', handleErrorAsync((req, res, next) => UserController.getUser(req, res, next)))
 
 // 登入
-router.post('/', UserController.login)
+router.post('/', handleErrorAsync((req, res, next) => UserController.login(req, res, next)))
+
+// 用戶註冊
+router.post('/signin', handleErrorAsync((req, res, next) => UserController.singin(req, res, next)))
 
 module.exports = router
