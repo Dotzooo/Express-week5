5bbd0b44df5d83eddde1e9e332aa01724ec2e753
Adam Hsu <projectnba77@gmail.com>
2022-06-02 11:39:18 +0800

    fix: user Route， handleErrorAsync 重複調用

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,12 @@ const handleErrorAsync = require("../service/handleErrorAsync");
 const UserController = require('../controllers/users')
 
 // 取得所有用戶
-router.get('/', handleErrorAsync((req, res, next) => UserController.getUser(req, res, next)))
+router.get('/', handleErrorAsync(UserController.getUser))
 
 // 登入
-router.post('/', handleErrorAsync((req, res, next) => UserController.login(req, res, next)))
+router.post('/', handleErrorAsync(UserController.login))
 
 // 用戶註冊
-router.post('/signin', handleErrorAsync((req, res, next) => UserController.singin(req, res, next)))
+router.post('/signin', handleErrorAsync(UserController.singin))
 
 module.exports = router
